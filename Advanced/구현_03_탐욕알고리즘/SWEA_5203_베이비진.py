74588ab6603ed94962d1d82de0d73e292c750af7
HEECO <33312417+phc5403@users.noreply.github.com>
2023-04-21 18:37:14 +0900

    Update SWEA_5203_베이비진.py

--- Advanced/구현_03_탐욕알고리즘/SWEA_5203_베이비진.py
+++ Advanced/구현_03_탐욕알고리즘/SWEA_5203_베이비진.py
@@ -11,7 +11,7 @@ def babygin(arr):
 
     # run
     for r in range(8):
-        if card[r] > 0 and card[r+1] > 0 and card[r+2] > 0:
+        if card[r] and card[r+1] and card[r+2]:
             return True
 
     return False
@@ -26,20 +26,10 @@ for tc in range(1, int(input()) + 1):
     for idx in range(3, 7):
         if babygin(A[:idx]):
             res = 1
-            print(f'#{tc} {res}')
             break
 
         if babygin(B[:idx]):
             res = 2
-            print(f'#{tc} {res}')
             break
 
-    # 무승부
-    if not res:
-        print(f'#{tc} {res}')
-        
-'''
-처음에 WA, Testcase(8/10) 한 번 났었음.
-문제를 잘 읽으면 A와 B의 모든 판단 후에 승자를 비교하는것이 아니라,
-A → B 차례로 "개별" 판단하다가 누구든 먼저 완성되는 순간 그대로 끝나야함.
-'''
+    print(f'#{tc} {res}')
